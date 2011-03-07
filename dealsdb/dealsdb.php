0819828a132bf5c89b022916f550a34563912aee
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-03-07 18:39:31 +0000

    small changes

--- dealsdb/dealsdb.php
+++ dealsdb/dealsdb.php
@@ -488,7 +488,7 @@ if ( is_admin() ) {
 
 // Initiate the plugin
 add_action("init", "DealsDbInit");
-add_action('admin_init' , 'addcustomtype_admin_styles'); 
+add_action('admin_print_styles' , 'addcustomtype_admin_styles'); 
 add_action('wp_print_styles' , 'addcustomtype_styles'); 
 add_action('save_post', "my_wp_insert_post");
 
